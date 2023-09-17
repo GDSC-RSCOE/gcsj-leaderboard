186d74a3b398acd27349a29ab1a1c64eadae401e
Himan-Miku <himanshu28.mj@gmail.com>
2023-09-17 19:56:11 +0530

    style: add responsiveness

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 <main class="apply-font">
   <div class="flex flex-col justify-center items-center">
     <div class="flex justify-center py-6">
-      <img src="gdsc-main-bg.png" alt="jam_image" class="w-[60%]" />
+      <img src="gdsc-main-bg.png" alt="jam_image" class="w-[80%] md:w-[60%]" />
     </div>
   </div>
   <DataTable />
--- src/DataTable.svelte
+++ src/DataTable.svelte
@@ -39,9 +39,9 @@
     );
 </script>
 
-<main class="flex flex-col gap-10 justify-center items-center">
+<main class="flex flex-col gap-6 md:gap-10 justify-center items-center">
   <div
-    class="min-h-fit min-w-fit border border-green-400 bg-green-100 rounded-xl text-center p-4 text-base font-semibold text-slate-500"
+    class="min-h-fit min-w-fit border border-green-400 bg-green-100 rounded-xl text-center md:p-4 p-3 md:text-base text-sm font-semibold text-slate-500"
   >
     No. of Total Completions : {calcCompletions()}
     <i class="fa-solid fa-gift text-green-600 pl-2" />
@@ -51,23 +51,27 @@
     type="text"
     placeholder="Search by Student Name"
     bind:value={searchQuery}
-    class="px-4 py-2 border-2 border-green-300 rounded-lg w-[55%] focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent focus:shadow-lg focus:transition-all duration-500 ease-in-out"
+    class="px-4 md:py-2 py-1 border-2 border-green-300 rounded-lg md:w-[55%] w-[50%] focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent focus:shadow-lg focus:transition-all duration-500 ease-in-out"
   />
 
   <div class="table-wrapper flex">
-    <table class="min-w-[85%]">
+    <table class="min-w-[85%] md:text-base text-sm">
       <thead class="text-center">
         <tr>
-          <th class="px-6 py-3 bg-gray-200 font-semibold w-[27%] rounded-ss-xl"
+          <th
+            class="px-6 py-3 bg-gray-200 font-semibold md:w-[27%] w-[65%] rounded-ss-xl"
             >Student Name</th
           >
-          <th class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[15%]"
+          <th
+            class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold md:w-[15%] w-[35%] rounded-se-xl"
             >Completion Status</th
           >
-          <th class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[15%]"
+          <th
+            class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[15%] md:table-cell hidden"
             >Redemption Status</th
           >
-          <th class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[15%]"
+          <th
+            class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[15%] md:table-cell hidden"
             ><button on:click={sortByCourses}>
               {#if sortByCoursesCompleted}
                 <span class="mr-2">Course Badges</span><i
@@ -80,11 +84,12 @@
               {/if}
             </button>
           </th>
-          <th class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[14%]"
+          <th
+            class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[14%] md:table-cell hidden"
             >Skill Badges</th
           >
           <th
-            class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[14%] rounded-se-xl"
+            class="px-6 py-3 bg-gray-200 text-gray-600 font-semibold w-[14%] rounded-se-xl md:table-cell hidden"
             >GenAI Game</th
           >
         </tr>
@@ -92,7 +97,7 @@
       <tbody class="border border-gray-100">
         {#each filteredData as entry, index (entry["Google Cloud Skills Boost Profile URL"])}
           <tr class={index % 2 === 0 ? "bg-gray-100" : "bg-white"}>
-            <td class="px-6 py-4 w-[27%] tracking-wide">
+            <td class="px-6 py-4 md:w-[27%] w-[65%] tracking-wide">
               <a
                 href={entry["Google Cloud Skills Boost Profile URL"]}
                 target="_blank"
@@ -103,27 +108,27 @@
                 {/if}
               </a>
             </td>
-            <td class="px-6 py-4 w-[15%] text-center">
+            <td class="px-6 py-4 md:w-[15%] w-[35%] text-center">
               {#if entry["Total Completions of both Pathways"] === "Yes"}
                 <i class="fas fa-circle-check text-green-500" />
               {:else}
                 <i class="fas fa-circle-xmark text-red-500" />
               {/if}
             </td>
-            <td class="px-6 py-4 w-[15%] text-center">
+            <td class="px-6 py-4 w-[15%] text-center md:table-cell hidden">
               {#if entry["Redemption Status"] === "Yes"}
                 <i class="fas fa-circle-check text-green-500" />
               {:else}
                 <i class="fas fa-circle-xmark text-red-500" />
               {/if}
             </td>
-            <td class="px-6 py-4 w-[15%] text-center"
+            <td class="px-6 py-4 w-[15%] text-center md:table-cell hidden"
               >{entry["# of Courses Completed"]}</td
             >
-            <td class="px-6 py-4 w-[14%] text-center"
+            <td class="px-6 py-4 w-[14%] text-center md:table-cell hidden"
               >{entry["# of Skill Badges Completed"]}</td
             >
-            <td class="px-6 py-4 w-[14%] text-center"
+            <td class="px-6 py-4 w-[14%] text-center md:table-cell hidden"
               >{entry["# of GenAI Game Completed"]}</td
             >
           </tr>
